1c1a1c0b5580a8234aa2056d3089616e013048c0
TomKry5821 <tomaszkrypczyk@gmail.com>
2021-11-27 23:20:14 +0100

    Allign columns in first matrix, align rows in second matrix, do file reading

--- MatrixMultiplicationAsm/MAtrixMultiplication.asm
+++ MatrixMultiplicationAsm/MAtrixMultiplication.asm
@@ -40,15 +40,19 @@ inc count
 			movq xmm0, qword ptr [R10]					; | move actual row element to vector
 			movq xmm4, qword ptr [R8]					; | move actual column element to vector
 			pmulld xmm0, xmm4							; | multiply vectors
-			paddq xmm2, xmm0							; | add result to third vector
+			pxor xmm3, xmm3
+			haddps xmm3, xmm0
+			paddq xmm2, xmm3							; | add result to third vector
 
-			add R10, 4									; | row++ 4
-			add R8, 4									; | columns ++ 4
+			add R10, 8									; | row++ 4
+			add R8, 8									; | columns ++ 4
 
 
 			dec ECX										; | decrementing loop counter
+			dec ECX
 			jnz loop2									; | if loopCount == 0 break
 
+pshufd xmm2, xmm2, 2
 movq RAX, xmm2											; |
 mov [R9], EAX											; | resultRow = rows * columns EAX
 add R9, 4												; | resultRows++ 4
